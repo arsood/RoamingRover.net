6326f02ab64e382e86feee276be06660c181a413
student <student@academyx.com>
2014-01-08 20:03:48 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RoamingRoverNet::Application.config.secret_key_base = 'e4ba002a02ff8f5da105ccec8304a1e887ae7352b642a802b28be60416df20a99d4380289b3b53a2b54bf97dc97fe53bafb367d2cf255a6576ca3735e78da9b3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RoamingRoverNet::Application.config.session_store :cookie_store, key: '_RoamingRover_net_session'
