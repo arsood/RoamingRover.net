dd488cf225a6fc2ddf109e92562587d708edfbfa
Asitha de Silva <asithade@gmail.com>
2014-01-18 17:18:55 -0800

    Added Validation

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -2,12 +2,10 @@ class DogsController < ApplicationController
 	def create 
 		@dog = Dog.new(dogs_params)
 		@dog.owner_id = current_user.owner.id
-		respond_to do |format|
-			if @dog.save
-				redirect_to owners_path, :notice => "Dog added successfully!"
-			else
-			    redirect_to owners_path, :alert => "We were not able to add your dog!"
-			end
+		if @dog.save
+			redirect_to owners_path, :notice => "Dog added successfully!"
+		else
+		    redirect_to owners_path, :alert => "We were not able to add your dog!"
 		end
 	end
 
--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,5 +1,5 @@
 class Dog < ActiveRecord::Base
   belongs_to :owner
   attr_accessible :name, :breed, :age, :image, :owner_id, :about
-  
+  validates :name, :breed, :age, presence: true
 end
