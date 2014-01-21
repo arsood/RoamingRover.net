4c9e3ef9d53e160e1c2c61e8756bd764ae03ff2f
Asitha de Silva <asithade@gmail.com>
2014-01-20 20:18:57 -0800

    Started on Job page

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,14 +2,19 @@ class JobsController < ApplicationController
 	def create
 		@job = Job.new(jobs_params)
 		@job.owner_id = current_user.owner.id
-		@job.dogs = params[:dogs].to_s
-		@job.days = params[:days].to_s
+		@job.dogs = params[:dogs]
+		@job.days = params[:days]
 		if @job.save
 			redirect_to owners_path, :notice => "Job added successfully!"
 		else
 		    redirect_to owners_path, :alert => "There was something wrong with your request!"
 		end
 	end
+
+	def show 
+		@job = Job.find(params[:id])
+	end
+
 	def jobs_params
 		params.require(:job).permit(:title, :description, :days, :rate, :owner_id, :status, :dogs)
 	end
