28c0d84e7910390cfd8197553ff091b7242476f5
Asitha de Silva <asithade@gmail.com>
2014-01-21 20:45:32 -0800

    UI Changes and Data Destruction

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,4 +1,6 @@
 class JobsController < ApplicationController
+	before_action :correct_owner, only: [:edit, :update, :destroy]
+
 	def create
 		@job = Job.new(jobs_params)
 		@job.owner_id = current_user.owner.id
@@ -14,6 +16,21 @@ class JobsController < ApplicationController
 	def show 
 		@job = Job.find(params[:id])
 	end
+
+	# DELETE /posts/1
+	# DELETE /posts/1.json
+	def destroy
+	  @job.destroy
+	  respond_to do |format|
+	    format.html { redirect_to owners_path }
+	    format.json { head :no_content }
+	  end
+	end
+
+    def correct_owner
+    	@job = current_user.owner.jobs.find(params[:id])
+        redirect_to owners_path if @job.nil?
+    end	
 
 	def jobs_params
 		params.require(:job).permit(:title, :description, :days, :rate, :owner_id, :status, :dogs)
