fb47bda9bfe512917f566bc44d17b0a57b2bae89
Dina Graves <dinataruni@gmail.com>
2014-01-23 18:36:24 -0800

    Adding dogwalkers listing to owners to owners dashboard

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,13 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+
+  Walker.create([{walker_id: "Walker 1", zipcode: "94110", age: '15', breeds: 'Big and little',
+    experience: '7 years', about: "I'm awesome at watching dogs"},
+    {walker_id: "Walker 2", zipcode: "94112", age: '25', breeds: 'Big and little',
+    experience: '7 years', about: "I'm awesome at watching dogs"},
+    {walker_id: "Walker 3", zipcode: "94113", age: '25', breeds: 'Big and little',
+    experience: '7 years', about: "I'm awesome at watching dogs"},
+    {walker_id: "Walker 4", zipcode: "94114", age: '35', breeds: 'Big and little',
+    experience: '7 years', about: "I'm awesome at watching dogs"}])
+
